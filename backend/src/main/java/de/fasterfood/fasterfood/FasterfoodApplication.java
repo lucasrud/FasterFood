08ca19f9e4e469ad19f84b31ae1b241fe2229c40
Adam Korkosz <adam.korkosz@gmx.de>
2019-12-09 12:18:27 +0100

    bla

---
+++ backend/src/main/java/de/fasterfood/fasterfood/FasterfoodApplication.java
@@ -0,0 +1,13 @@
+package de.fasterfood.fasterfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FasterfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FasterfoodApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/fasterfood/fasterfood/FasterfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fasterfood.fasterfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FasterfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

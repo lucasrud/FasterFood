7b4d9323ebfe4afbeca522d0ffdb3c3e894082bc
Adam Korkosz <adam.korkosz@gmx.de>
2019-12-10 12:59:49 +0100

    //Todo: implement Observable for currentĆost

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -30,7 +30,8 @@ export class AppComponent implements OnInit {
   }
 
   addProcess(mealName: Meal) { // TODO erstmal Platzhalter nur mit Strings für die angeklickten Waren, später dann mit Process?
-    this.orderService.addTempProcessList(mealName);
+    this.orderService.addToMealList(mealName);
+    this.orderService.setOrderCost();
   }
 
 }

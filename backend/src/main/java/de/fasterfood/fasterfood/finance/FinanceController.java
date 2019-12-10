39bca1ed97013bfb6df1cc36fc6fbc46fcbab592
Tim <tim@jacobsenonline.de>
2019-12-10 12:49:00 +0100

    FinanceController and Service added

---
+++ backend/src/main/java/de/fasterfood/fasterfood/finance/FinanceController.java
@@ -0,0 +1,25 @@
+package de.fasterfood.fasterfood.finance;
+
+import de.fasterfood.fasterfood.process.Process;
+import de.fasterfood.fasterfood.process.ProcessRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+public class FinanceController {
+    ProcessRepository processRepository;
+
+    @Autowired
+    public FinanceController(ProcessRepository processRepository){
+        this.processRepository = processRepository;
+    }
+
+    @GetMapping("finance/turnover")
+    public List<Process> dailyTurnover(){
+        return processRepository.findAllByOrderDate(LocalDate.now());
+    }
+}
---
+++ backend/src/main/java/de/fasterfood/fasterfood/finance/FinanceService.java
@@ -0,0 +1,7 @@
+package de.fasterfood.fasterfood.finance;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FinanceService {
+}

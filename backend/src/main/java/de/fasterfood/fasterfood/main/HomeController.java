766e650c5b22455c2cc41d9b2e8df9e55847bd03
Adam Korkosz <adam.korkosz@gmx.de>
2019-12-10 10:39:06 +0100

    postconstruct addition

--- backend/src/main/java/de/fasterfood/fasterfood/main/HomeController.java
+++ backend/src/main/java/de/fasterfood/fasterfood/main/HomeController.java
@@ -45,52 +45,54 @@ public class HomeController {
     @PostConstruct
     public void setupData(){
 
-        Ingredient cowMeat = new Ingredient("cowMeat", 0.4, 20);
-        Ingredient salad = new Ingredient("cowMeat", 0.4, 20);
-        Ingredient pita = new Ingredient("pita", 0.4, 20);
-        Ingredient sauce = new Ingredient("sauce", 0.4, 20);
-        Ingredient veggies = new Ingredient("veggies", 0.4, 20);
-        Ingredient dough = new Ingredient("dough", 0.4, 20);
+        if (mealRepository.count() == 0) {
+            Ingredient cowMeat = new Ingredient("cowMeat", 0.4, 20);
+            Ingredient salad = new Ingredient("cowMeat", 0.4, 20);
+            Ingredient pita = new Ingredient("pita", 0.4, 20);
+            Ingredient sauce = new Ingredient("sauce", 0.4, 20);
+            Ingredient veggies = new Ingredient("veggies", 0.4, 20);
+            Ingredient dough = new Ingredient("dough", 0.4, 20);
 
-        List<Ingredient> ingredients = new LinkedList<>();
-        ingredients.add(cowMeat);
-        ingredients.add(salad);
-        ingredients.add(pita);
-        ingredients.add(sauce);
-        ingredients.add(veggies);
+            List<Ingredient> ingredients = new LinkedList<>();
+            ingredients.add(cowMeat);
+            ingredients.add(salad);
+            ingredients.add(pita);
+            ingredients.add(sauce);
+            ingredients.add(veggies);
 
-        Meal kebab = new Meal("kebab", 6, ingredients);
-        Meal wrap = new Meal("pizza", 6, ingredients);
-        Meal turkishPizza = new Meal("turkishPizza", 6, ingredients);
-        Meal durum = new Meal("durum", 6, ingredients);
-        Meal crog = new Meal("crog", 6, ingredients);
+            Meal kebab = new Meal("kebab", 6, ingredients);
+            Meal wrap = new Meal("pizza", 6, ingredients);
+            Meal turkishPizza = new Meal("turkishPizza", 6, ingredients);
+            Meal durum = new Meal("durum", 6, ingredients);
+            Meal crog = new Meal("crog", 6, ingredients);
 
-        Process process0 = new Process(kebab, kebab.getRetailPrice());
-        Process process1 = new Process(kebab, kebab.getRetailPrice());
+            Process process0 = new Process(kebab, kebab.getRetailPrice());
+            Process process1 = new Process(kebab, kebab.getRetailPrice());
 
-        List<Process> processes = new LinkedList<>();
-        processes.add(process0);
-        processes.add(process1);
+            List<Process> processes = new LinkedList<>();
+            processes.add(process0);
+            processes.add(process1);
 
-        LocalDate today = LocalDate.now();
-        LocalTime now = LocalTime.now();
+            LocalDate today = LocalDate.now();
+            LocalTime now = LocalTime.now();
 
-        Order order = new Order(today, now, processes);
+            Order order = new Order(today, now, processes);
 
-        for (Ingredient ingredient : ingredients){
-            ingredientRepository.save(ingredient);
+            for (Ingredient ingredient : ingredients){
+                ingredientRepository.save(ingredient);
+            }
+            mealRepository.save(kebab);
+            mealRepository.save(turkishPizza);
+            mealRepository.save(crog);
+            mealRepository.save(durum);
+            mealRepository.save(wrap);
+
+            for (Process process : processes){
+                processRepository.save(process);
+            }
+
+            orderRepository.save(order);
         }
-        mealRepository.save(kebab);
-        mealRepository.save(turkishPizza);
-        mealRepository.save(crog);
-        mealRepository.save(durum);
-        mealRepository.save(wrap);
-
-        for (Process process : processes){
-            processRepository.save(process);
-        }
-
-        orderRepository.save(order);
 
     }
 }
--- backend/src/main/java/de/fasterfood/fasterfood/security/WebSecurityConfiguration.java
+++ backend/src/main/java/de/fasterfood/fasterfood/security/WebSecurityConfiguration.java
@@ -12,6 +12,6 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests().antMatchers("/","/fasterfood/order","/fasterfood").permitAll();
-        
+
     }
 }

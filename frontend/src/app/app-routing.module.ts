891a9955b15377542480b4db48ac994ef9dcfbbb
Lucas <lucasrudolf@web.de>
2019-12-12 12:48:27 +0100

    separated the meals from app

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MealsComponent} from './meals/meals.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'edit-meals', component: MealsComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/meals/meals.component.ts
+++ frontend/src/app/meals/meals.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
+import { OrderService} from '../order.service';
+import {TestService} from '../testservice';
+import {Meal} from '../meal';
+import {MealDTO} from '../mealDTO';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-meals',
   templateUrl: './meals.component.html',
-  styleUrls: ['./meals.component.css']
+  styleUrls: ['./meals.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class MealsComponent implements OnInit {
 
-  constructor() { }
+  newMeals: MealDTO[];
+  name = '';
+  @Input()
+  meals: Meal[];
+  orderService: OrderService;
+  testService: TestService;
 
-  ngOnInit() {
+  constructor(private http: HttpClient, orderService: OrderService, testService: TestService) {
+    this.orderService = orderService;
+    this.testService = testService;
   }
 
+  ngOnInit(): void {
+    this.http.get<Meal[]>('/api/fasterfood/order').subscribe(meals => this.meals = meals);
+    // Diese Methoden sollten bei Gelegenheit in den/ einen Service ausgelagert werden? AK
+    this.resetNewMeal();
+    this.newMeals = [];
+  }
+
+  addMeal(nameE, priceE) {
+    const m: MealDTO = {
+      name: nameE,
+      price: priceE,
+    };
+    console.log('aa');
+    this.http.post<Meal[]>('/api/fasterfood/addMeal', m).subscribe(meals => this.meals = meals);
+    this.newMeals.push(m);
+    this.resetNewMeal();
+  }
+
+  deleteMeal(mealToBeDeleted) {
+    this.http.post<Meal[]>('/api/fasterfood/deleteMeal', mealToBeDeleted).subscribe(meals => this.meals = meals);
+  }
+
+  resetNewMeal() {
+    this.name = '';
+  }
 }

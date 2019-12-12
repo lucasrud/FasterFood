608a35a7d5f1f12d291fc07cc424d41f4fcdd9b6
Adam Korkosz <adam.korkosz@gmx.de>
2019-12-12 14:32:06 +0100

    cashregister created, reworked mainscreen and outsourced components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {MealsComponent} from './meals/meals.component';
+import {CashregisterComponent} from './cashregister/cashregister.component';
 
 
 const routes: Routes = [
-  { path: 'edit-meals', component: MealsComponent }];
+  { path: '', component: CashregisterComponent },
+  { path: 'edit-meals', component: MealsComponent }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

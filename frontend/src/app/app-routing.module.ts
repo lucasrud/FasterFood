7c6584d1ced9f259668a3f7fa0f33fa83d5397ef
idaja <idajaganjac@yahoo.de>
2019-12-12 12:51:13 +0100

    added router outlet tags

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {IngredientsComponent} from './ingredients/ingredients.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'ingredients', component: IngredientsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

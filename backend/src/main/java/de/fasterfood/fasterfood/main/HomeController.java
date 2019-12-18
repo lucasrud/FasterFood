b65fab136d8411429efd276ee6e7a2a4a6e60444
idaja <idajaganjac@yahoo.de>
2019-12-18 09:16:15 +0100

    cleaned up post construct

--- backend/src/main/java/de/fasterfood/fasterfood/main/HomeController.java
+++ backend/src/main/java/de/fasterfood/fasterfood/main/HomeController.java
@@ -64,11 +64,7 @@ public class HomeController {
             Ingredient garlicSauce = new Ingredient("Garlic sauce", 0.4, 400);
             Ingredient spicySauce = new Ingredient("Spicy sauce", 0.4, 500);
 
-            Ingredient test1 = new Ingredient("DeleteableNoDependency1", 0.25, 750);
-            Ingredient test2 = new Ingredient("DeleteableNoDependency2", 0.12, 1200);
-            Ingredient test3 = new Ingredient("DeleteableNoDependency3", 0.07, 380);
-            Ingredient test4 = new Ingredient("DeleteableNoDependency4", 0.03, 8400);
-            Ingredient wasserflasche = new Ingredient("Bottle of Water - Bonaqua", 0.03, 8400);
+            Ingredient wasserflasche = new Ingredient("Bottle of Water - Bonaqua", 0.03, 400);
 
 
 
@@ -123,10 +119,7 @@ public class HomeController {
                 ingredientRepository.save(ingredient);
             }
 
-            ingredientRepository.save(test1);    // without dependency to a meal
-            ingredientRepository.save(test2);    // without dependency to a meal
-            ingredientRepository.save(test3);    // without dependency to a meal
-            ingredientRepository.save(test4);    // without dependency to a meal
+
             ingredientRepository.save(wasserflasche);    // has dependency to Water
 
             mealRepository.save(beef_kebab);
